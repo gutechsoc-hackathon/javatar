a8f37c47c5a686c68949d5717e01a58b7e76bf94
Georgi Tsaklev <y0c0vc@gmail.com>
2013-11-02 10:00:30 +0000

    Graph basic methods

--- GU_hack_SAS/src/Graph.java
+++ GU_hack_SAS/src/Graph.java
@@ -3,5 +3,17 @@ import java.util.HashMap;
 
 public class Graph {
 
-	public HashMap<Long, Node> fileMap = new HashMap<Long,Node>();
+	public HashMap<Long, Node> fileMap;
+	
+	public Graph(){
+		 fileMap = new HashMap<Long,Node>();
+	}
+	
+	public long getSize(){
+		return fileMap.size();
+	}
+	
+	public void averageRelationships(){
+		
+	}
 }
