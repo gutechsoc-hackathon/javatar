d9f963011d1c6a3b61052e126ca2ecc7f94ea9ec
mp3por <v.b.k.2007@abv.bg>
2013-11-02 11:23:42 +0000

    Runners full 1

--- GU_hack_SAS/src/Runner.java
+++ GU_hack_SAS/src/Runner.java
@@ -4,11 +4,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 public class Runner {
+	public static final Graph graph = new Graph();
+	public static int hasReleationshipWithHimself = 0;
+	public static int duplicate = 0;
 	public static void main(String[] args){
 		try {
 			BufferedReader in = new BufferedReader(new FileReader("relationships-small.txt"));
 			String line = null;
+			String mainId = null;
 			while((line = in.readLine()) !=null){
 				line = line.trim();
 				if(!line.isEmpty()){
@@ -18,20 +24,61 @@ public class Runner {
 							System.out.println("SKOBA: " + splittedLine[0]);
 						}else{
 							System.out.println("ID :" + splittedLine[0]);
+							mainId = splittedLine[0];
+							Runner.handleIdEntry(mainId);
 						}  
 					}else{
-						System.out.println(splittedLine.length);
+						//System.out.println(splittedLine.length);
+						if(splittedLine.length == 2 ){
+							Runner.isInRelationshipWithHimself(mainId, splittedLine[1]);
+							//TODO: handle relationships
+							try {
+								Runner.addRelationship(mainId, splittedLine[0], splittedLine[1]);
+							} catch (NumberFormatException e) {
+								// TODO: handle incorrect relation ID 
+								System.out.println("INCORRECT RELATION ID -> line : " + line);
+							}
+							
+						}else{
+							//TODO: handle incorrect input
+							System.out.println("INCORRECT INPUT");
+						}
+						
 					}
 				}
 			}
-			Graph graph = new Graph ();
 		} catch (FileNotFoundException e) {
 			System.out.println("ne 4ete ot faila -> v Runner");
 			e.printStackTrace();
 		} catch (IOException e) {
 			System.out.println("ne6to se barka s 4eteneto na liniq");
 			e.printStackTrace();
+		} 
+	}
+	public static void isInRelationshipWithHimself(String a, String b){
+		if(a == b){
+			hasReleationshipWithHimself++;
 		}
-		
+	}
+	public static void handleIdEntry(String mainId){
+		if (graph.getFileMap().containsKey(Long.parseLong(mainId))){
+			duplicate++;
+			//TODO: handle update of old entry
+		}else{
+			graph.getFileMap().put(Long.parseLong(mainId), new Node(Long.parseLong(mainId)));
+		}
+	}
+	public static void addRelationship(String mainId, String relationship, String relId)throws NumberFormatException{
+		if(relationship.compareToIgnoreCase("dislikes")==0){
+			graph.addDislike(Long.parseLong(mainId), Long.parseLong(relId));
+		}else if (relationship.compareToIgnoreCase("friend_of")==0){
+			graph.addFriend(Long.parseLong(mainId), Long.parseLong(relId));
+		}else if (relationship.compareToIgnoreCase("knows")==0){
+			graph.addKnows(Long.parseLong(mainId), Long.parseLong(relId));
+		}else if (relationship.compareToIgnoreCase("married_to")==0){
+			graph.addMarried(Long.parseLong(mainId), Long.parseLong(relId));
+		}else if (relationship.compareToIgnoreCase("has_dated")==0){
+			graph.addDated(Long.parseLong(mainId), Long.parseLong(relId));
+		}
 	}
 }
