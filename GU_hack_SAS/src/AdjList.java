e38abe33b79d8d3b2e332d85b8b34cffd0664337
Radko Kotev <radkokotev@gmail.com>
2013-11-02 09:55:30 +0000

    Basic structure of AdjList and AdjNode

--- GU_hack_SAS/src/AdjList.java
+++ GU_hack_SAS/src/AdjList.java
@@ -1,4 +1,18 @@
+import java.util.HashSet;
+
 
 public class AdjList {
-
+	private HashSet<AdjListNode> list;
+	
+	public AdjList() {
+		this.list = new HashSet<AdjListNode>();
+	}
+	
+	public void add(long id) {
+		this.list.add(new AdjListNode(id));
+	}
+	
+	public void add(AdjListNode node) {
+		this.list.add(node);
+	}
 }
--- GU_hack_SAS/src/AdjListNode.java
+++ GU_hack_SAS/src/AdjListNode.java
@@ -1,4 +1,16 @@
 
 public class AdjListNode {
-
+	private long id;
+	
+	public AdjListNode(long id) {
+		this.id = id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public long getId() {
+		return this.id;
+	}
 }
