9709e6d1829bd96fcb1f66938c07a8da8fed263b
Georgi Tsaklev <y0c0vc@gmail.com>
2013-11-02 09:48:29 +0000

    Defined hashmap in Graph and varriables in Node file

--- GU_hack_SAS/src/Graph.java
+++ GU_hack_SAS/src/Graph.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
+
 
 public class Graph {
 
+	public HashMap<Long, Node> fileMap = new HashMap<Long,Node>();
 }
--- GU_hack_SAS/src/Node.java
+++ GU_hack_SAS/src/Node.java
@@ -2,5 +2,9 @@
 public class Node {
 	
 	public AdjList DISLIKES = new AdjList();
+	public AdjList FRIEND_OF = new AdjList();
+	public AdjList KNOWS = new AdjList();
+	public AdjList MARRIED_TO = new AdjList();
+	public AdjList HAS_DATED = new AdjList();
 
 }
